a8f4c0f13a255467893e8dc6bf0d04e9aa6be39c
abhiprd-219 <78641798+abhiprd-219@users.noreply.github.com>
2021-03-29 22:05:12 +0530

    Create .js

---
+++ .js
@@ -0,0 +1,14 @@
+    $(function() {
+       $("#newadd").on("click", function() {
+         var val = $("input").val();
+           if(val !== '') {
+            var elem = $("<li></li>").text(val);
+            $(elem).append("<button class='rem'>X</button>");
+            $("#list").append(elem);
+            $("input").val("");
+            $(".rem").on("click", function() {
+                $(this).parent().remove();
+            });
+        }
+       });
+     });
